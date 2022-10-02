e6b2d8ff8148ebe74fa647f5b849c337f121c7a8
Max <0665232@mail.ru>
2022-10-02 22:16:15 +0300

    Вторая задача решена

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿// Задача 1. Какое число больше, какое меньше.
-
+/*
 Console.WriteLine("Введите первое целое число");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе целое число");
@@ -19,4 +19,28 @@ int b = Convert.ToInt32(Console.ReadLine());
 {
         Console.WriteLine("Второе число больше первого");
 }
+*/
 
+// Задача 2. Дано 3 числа, вывести большое.
+
+Console.WriteLine("Введите первое целое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третее целое число");
+int c = Convert.ToInt32(Console.ReadLine());
+//int max=0; Попробовать потом сделать цикл
+
+    if (a>b&a>c)
+{
+        Console.WriteLine("Первое число больше останльных");
+}
+    else if (b>a&b>c)
+{
+        Console.WriteLine("Второе число больше остальных");
+}
+    else 
+{
+        Console.WriteLine("Третее число больше остальных");
+}
+    
