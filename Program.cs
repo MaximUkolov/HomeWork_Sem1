f74b90a990d38cbde7dbd0149716327f905f3cad
Max <0665232@mail.ru>
2022-10-02 21:49:03 +0300

    Первая задача решена

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
 ﻿// Задача 1. Какое число больше, какое меньше.
 
+Console.WriteLine("Введите первое целое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число");
+int b = Convert.ToInt32(Console.ReadLine());  
+   
+    if 
+    (a > b)
+{
+        Console.WriteLine("Первое число больше второго");
+}   
+
+    else if (a==b) 
+{    
+    Console.WriteLine("Числа равны");
+}
+    else
+{
+        Console.WriteLine("Второе число больше первого");
+}
+
