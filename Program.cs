aa0849c32f9a0c153ee81c0fe9db0fd3bf977789
Max <0665232@mail.ru>
2022-10-05 22:37:35 +0300

    Задача 10 готова

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 10. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int a = new Random().Next(100,999);
+Console.WriteLine(a);
+int first = a / 100;
+int second = (a % 100) / 10;
+int therd = a % 10;
+
+Console.WriteLine(second);
